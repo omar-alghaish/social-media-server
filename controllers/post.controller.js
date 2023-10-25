d3f61288f6cf64c5c2cc5c99126fa5a79d80d992
omar-alghaish <omaralghish@gmail.com>
2023-10-25 21:15:28 +0300

    any

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -11,12 +11,27 @@ export const createPost = asyncHandler(async (req, res, next) => {
     content: req.body.content,
     media: mediaFiles,
   });
-  res.status(201).json({ data: newPost });
+  res.status(200).json({ data: newPost });
 });
 
-export const getUserPost = asyncHandler(async(req,res,next)=>{
-  const {id} = req.params;
-  const posts = await Post.find({user:id})
-  res.status(201).json({ data: posts });
+export const getUserPost = asyncHandler(async (req, res, next) => {
+  const { id } = req.params;
+  const posts = await Post.find({ user: id });
+  res.status(200).json({ data: posts });
+});
 
-})
+export const likePost = asyncHandler(async (req, res, next) => {
+  const { postId } = req.body;
+  const post = await Post.findById(postId);
+  if (post.likes.includes(req.user._id)) {
+    let index = post.likes.indexOf(req.user._id);
+    if (index !== -1) {
+      post.likes.splice(index, 1);
+    }
+  }else{
+      post.likes.push(req.user._id);
+
+  }
+  await post.save();
+  res.status(200).json({ data: post.likes });
+});
--- routes/post.route.js
+++ routes/post.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { upload } from "../middlewares/uploadImage.middlesare.js";
-import { createPost, getUserPost } from "../controllers/post.controller.js";
+import { createPost, getUserPost, likePost } from "../controllers/post.controller.js";
 import { protect } from "../controllers/auth.controller.js";
 
 
@@ -8,5 +8,6 @@ const router = express.Router()
 
 router.post('/create',protect, upload.array('media', 5), createPost);
 router.get("/getPosts/:id",getUserPost)
+router.post("/like",protect,likePost)
 
 export default router
